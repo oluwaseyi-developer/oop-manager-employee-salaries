0195de4a3957df4e4d6f96facf2c379eb6eda338
oluwaseyi-developer <abidemiadetogun@gmail.com>
2024-05-01 19:20:52 +0100

    project completed

---
+++ manager.js
@@ -0,0 +1,47 @@
+const Employee = require("./employee");
+
+class Manager extends Employee {
+    constructor(name, salary, title, manager,employees = []) {
+        super(name, salary, title, manager)
+        this.employees = employees;
+
+        
+    }
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    } 
+
+    calculateBonus(multiplier) {
+        const totalSubSalary = this._totalSubSalary();
+        return totalSubSalary * multiplier
+    }
+
+    _totalSubSalary() {
+        let sum = 0;
+
+        this.employees.forEach(employee=> {
+            if(employee instanceof Manager) {
+                sum += employee._totalSubSalary();
+            } else {
+                sum += employee.salary; 
+            }
+        });
+        
+        return sum;
+    }
+}
+
+module.exports = Manager;
+
+
+
+const splinter = new Manager('Splinter', 100000, 'Sensei');
+const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
+
+console.log(splinter.calculateBonus(0.05)); // => 22500
+console.log(leo.calculateBonus(0.05)); // => 17500
+console.log(raph.calculateBonus(0.05)); // => 13000
